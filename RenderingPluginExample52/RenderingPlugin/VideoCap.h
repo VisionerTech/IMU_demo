431710ca98bba05ea6ba74fad4eba0b9455c2c2f
flankechen <flankechen@gmail.com>
2017-03-16 12:23:59 +0800

    first commit
    first commit

---
+++ RenderingPluginExample52/RenderingPlugin/VideoCap.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#ifndef __VIDEOSOURCE_H
+#define __VIDEOSOURCE_H
+
+//video input library to open cameras. unity side don't need to input camera index like 0 1.
+#define VIDEO_INPUT_LIB 1
+#if VIDEO_INPUT_LIB
+#endif
+
+#if VIDEO_INPUT_LIB
+#include "videoInput.h"
+#endif
+
+#define OPENCV 1
+#if OPENCV
+#endif
+
+#if OPENCV
+#include <opencv2\opencv.hpp>
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+
+//image size for ar mod rendering
+//current PTAM-demo machine camera setup is flipped 90.
+#define OPENCV_VIDEO_W 1080
+#define OPENCV_VIDEO_H 1080
+
+////image size for tracking, usually smaller for faster frame rate.
+//#define TRACK_IMAGE_W 480
+//#define TRACK_IMAGE_H 540
+
+//#define LOCAL_VIDEO
+#ifdef LOCAL_VIDEO
+//#define FRAME_BY_FRAME
+#endif
+
+#include "imu_parity_check.h"
+
+//camera fliped 90 for current 
+//#define CAMERA_FLIP
+
+
+class VideoSource
+{
+public:
+	//members
+#ifdef VIDEO_INPUT_LIB
+	videoInput VI;
+	int left_index;
+	int right_index;
+#else
+	cv::VideoCapture cap;
+	cv::VideoCapture cap_right;
+#endif
+
+	cv::Mat src;
+	//cv::Mat src_right;
+
+	cv::Mat src_flip;
+	//cv::Mat src_flip_right;
+
+	cv::Mat frame_rectify;
+	//cv::Mat frame_rectify_right;
+	
+//#if TRACK_IMAGE_W != OPENCV_VIDEO_W
+//	cv::Mat frame_rectify_down;
+//	cv::Mat frame_rectify_right_down;
+//#endif
+
+	//cv::Mat mx1, my1, mx2, my2;
+	//cv::Mat mx1_f, my1_f, mx2_f, my2_f;
+
+	//public functions
+	VideoSource();
+#ifdef VIDEO_INPUT_LIB
+	bool open_camera();
+#else
+	bool open_webcam(int index_0, int index_1);
+#endif
+	//bool open_webcam(int index_0, int index_1);
+	//bool open_webcam_mono();
+	//bool read_calib();
+	~VideoSource();
+	cv::Mat get_left_rgba();
+	//cv::Mat get_right_rgba();
+
+	//get imu data
+	std::vector<float> get_imu();
+
+};
+
+
+#endif
+
+
+#endif
---
+++ RenderingPluginExample52/UnityProject/Assets/UseRenderingPlugin.cs
@@ -0,0 +1,171 @@
+// on OpenGL ES there is no way to query texture extents from native texture id
+#if (UNITY_IPHONE || UNITY_ANDROID) && !UNITY_EDITOR
+	#define UNITY_GLES_RENDERER
+#endif
+
+
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+
+public class UseRenderingPlugin : MonoBehaviour
+{
+	// Native plugin rendering events are only called if a plugin is used
+	// by some script. This means we have to DllImport at least
+	// one function in some active script.
+	// For this example, we'll call into plugin's SetTimeFromUnity
+	// function and pass the current time so the plugin can animate.
+
+#if UNITY_IPHONE && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+#else
+	[DllImport ("RenderingPlugin")]
+#endif
+	private static extern void SetTimeFromUnity(float t);
+
+
+	// We'll also pass native pointer to a texture in Unity.
+	// The plugin will fill texture data from native code.
+#if UNITY_IPHONE && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+#else
+	[DllImport ("RenderingPlugin")]
+#endif
+#if UNITY_GLES_RENDERER
+	private static extern void SetTextureFromUnity(System.IntPtr texture, int w, int h);
+#else
+	private static extern void SetTextureFromUnity(System.IntPtr texture);
+#endif
+
+
+#if UNITY_IPHONE && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+#else
+	[DllImport("RenderingPlugin")]
+#endif
+	private static extern void SetUnityStreamingAssetsPath([MarshalAs(UnmanagedType.LPStr)] string path);
+
+
+#if UNITY_IPHONE && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+#else
+	[DllImport("RenderingPlugin")]
+#endif
+	private static extern IntPtr GetRenderEventFunc();
+
+	//native function for open web cam.
+	#if UNITY_IPHONE && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+	#else
+	[DllImport ("RenderingPlugin")]
+	#endif
+	private static extern Boolean OpenWebCam();
+
+	//native function call for destroying web cam
+	#if UNITY_IPHONE && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+	#else
+	[DllImport ("RenderingPlugin")]
+	#endif
+	private static extern void DestroyWebCam();
+
+	//native function call for getting return imu quaterion vector length
+	#if UNITY_IPHONE && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+	#else
+	[DllImport ("RenderingPlugin")]
+	#endif
+	private static extern int GetIMUQuaterionLength();
+
+	//native function for getting return imu quaterion array
+	#if UNITY_IPHONE && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+	#else
+	[DllImport ("RenderingPlugin", EntryPoint="GetQuaterionVector")]
+	#endif
+	private static extern IntPtr GetQuaterionVector();
+
+	private float[] imu_q_array = new float[4];
+	public GameObject main_camera;
+
+	IEnumerator Start()
+	{
+		if (OpenWebCam ()) {
+			Debug.Log("init success!");
+		}else {
+			Debug.Log("init false");
+		}
+
+		main_camera = GameObject.Find ("Main Camera");
+
+		SetUnityStreamingAssetsPath(Application.streamingAssetsPath);
+
+		CreateTextureAndPassToPlugin();
+		yield return StartCoroutine("CallPluginAtEndOfFrames");
+	}
+
+	private void CreateTextureAndPassToPlugin()
+	{
+		// Create a texture
+		Texture2D tex = new Texture2D(256,256,TextureFormat.ARGB32,false);
+		// Set point filtering just so we can see the pixels clearly
+		tex.filterMode = FilterMode.Point;
+		// Call Apply() so it's actually uploaded to the GPU
+		tex.Apply();
+
+		// Set texture onto our matrial
+		GetComponent<Renderer>().material.mainTexture = tex;
+
+		// Pass texture pointer to the plugin
+	#if UNITY_GLES_RENDERER
+		SetTextureFromUnity (tex.GetNativeTexturePtr(), tex.width, tex.height);
+	#else
+		SetTextureFromUnity (tex.GetNativeTexturePtr());
+	#endif
+	}
+
+	private IEnumerator CallPluginAtEndOfFrames()
+	{
+		while (true) {
+			// Wait until all frame rendering is done
+			yield return new WaitForEndOfFrame();
+
+			// Set time for the plugin
+			SetTimeFromUnity (Time.timeSinceLevelLoad);
+
+			// Issue a plugin event with arbitrary integer identifier.
+			// The plugin can distinguish between different
+			// things it needs to do based on this ID.
+			// For our simple plugin, it does not matter which ID we pass here.
+			GL.IssuePluginEvent(GetRenderEventFunc(), 1);
+
+
+			int quaterion_vec_length = GetIMUQuaterionLength ();
+			Debug.Log ("quaterion vector length" + quaterion_vec_length);
+
+			if (quaterion_vec_length > 4) {
+				IntPtr ptr = GetQuaterionVector ();
+				Marshal.Copy (ptr, imu_q_array, 0, 4);
+//				Debug.Log ("w:" + imu_q_array [0]);
+//				Debug.Log ("x:" + imu_q_array [1]);
+//				Debug.Log ("y:" + imu_q_array [2]);
+//				Debug.Log ("z:" + imu_q_array [3]);
+
+				Quaternion imu_q = new Quaternion (imu_q_array [1], -imu_q_array [3], imu_q_array [2], imu_q_array [0]);
+
+				main_camera.transform.rotation = imu_q;
+
+				main_camera.transform.Rotate (Vector3.right * -90.0f);
+			}
+        }
+	}
+
+	void OnApplicationQuit()
+	{
+		DestroyWebCam ();
+		Debug.Log("quit");
+	}
+
+}
